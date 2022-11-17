5bc9be5f25eeae376324d8827b63e1105ee5a510
oto0559 <117476701+oto0559@users.noreply.github.com>
2022-11-17 13:46:18 +0000

    Update SimuServer.py

--- SimuServer.py
+++ SimuServer.py
@@ -2,6 +2,7 @@ import socket  # Hlavní modul pro funkci serveru
 from threading import Thread  # Více procesů zároveň
 from datetime import datetime
 
+
 def accept_connection():
     while True:
         print(f'[*] Čekám na připojení {SERVER_HOST}:{SERVER_PORT}')
@@ -15,9 +16,9 @@ def accept_connection():
 def log_client(client):
 
     try:
-        name = client.recv(1024).decode("utf8")
+        name = client.recv(1024).decode('utf8')
         welcome = f'Vítej {name}'
-        client.send(bytes(welcome, "utf8"))
+        client.send(bytes(welcome, 'utf8'))
         msg = f'{name} se připojil.'
         broadcast(bytes(msg, 'utf8'))
         clients[client] = name
@@ -31,10 +32,10 @@ def log_client(client):
         date_now = datetime.now().strftime('%d-%m-%Y | %H:%M:%S')
         datename = f"[{date_now}] {name}"  # Formátování zprávy
         with open('chatlog.txt', 'a', encoding='utf-8') as log:  # Otevře log soubor
-            log.write(datename + str(msg) + '\n')  # Zapisuje zprávu do logu
+            log.write(datename + ': ' + msg.decode('utf8') + '\n')  # Zapisuje zprávu do logu
             log.close()
         if msg != bytes("quit", "utf8"):
-            broadcast(msg, datename+": ")
+            broadcast(msg, datename+": ")  # Posílá zprávu
         else:
             #client.send(bytes("quit", "utf8"))
             client.close()
@@ -42,7 +43,6 @@ def log_client(client):
             del clients[client]
             broadcast(bytes(f'{name} se odpojil.', encoding='utf8'))
             break
-
 
 
 def broadcast(msg, prefix=""):  # Pošle zprávu všem klientům
@@ -55,10 +55,10 @@ def broadcast(msg, prefix=""):  # Pošle zprávu všem klientům
 
 
 if __name__ == '__main__':
-    # IP adresa serveru
     clients = {}
     addresses = {}
     clientslist = []
+    # IP adresa serveru
     SERVER_HOST = "0.0.0.0"
     SERVER_PORT = 5003
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Pouze přejmenované na server pro lepší vyznání
