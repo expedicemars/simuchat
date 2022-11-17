12cd08a98af5a99b6b38ec32ce40f881fe87a1c1
oto0559 <117476701+oto0559@users.noreply.github.com>
2022-11-17 00:04:20 +0000

    Update SimuServer.py

--- SimuServer.py
+++ SimuServer.py
@@ -1,67 +1,72 @@
 import socket  # Hlavní modul pro funkci serveru
-import time
 from threading import Thread  # Více procesů zároveň
-from datetime import datetime  # Pro zaznamenání času odeslání zprávy
+from datetime import datetime
 
-
-def recv(conn, address):
-    print("Připojil se: " + str(address))
+def accept_connection():
     while True:
-        try:
-            data = conn.recv(1024).decode()  # Zkouší obdržet zprávu
+        print(f'[*] Čekám na připojení {SERVER_HOST}:{SERVER_PORT}')
+        client, client_address = server_socket.accept()  # Přijme nové připojení
+        print(f'[*]{client_address} se připojil.')
+        client.send(bytes('Spojení se serverem bylo navázáno, pro vypnutí napiš quit, nyní vlož své jméno:', 'utf8'))
+        addresses[client] = client_address
+        Thread(target=log_client, args=(client,)).start()
 
-            log = open('chatlog.txt', 'a', encoding='utf-8')  # Otevře log soubor
-            log.write(str(data) + '\n')  # Zapisuje zprávu do logu
+
+def log_client(client):
+
+    try:
+        name = client.recv(1024).decode("utf8")
+        welcome = f'Vítej {name}'
+        client.send(bytes(welcome, "utf8"))
+        msg = f'{name} se připojil.'
+        broadcast(bytes(msg, 'utf8'))
+        clients[client] = name
+
+    except ConnectionResetError:
+        print(f'Klient se nepodařilo připojit.')
+        client.close()
+
+    while True:
+        msg = client.recv(1024)
+        date_now = datetime.now().strftime('%d-%m-%Y | %H:%M:%S')
+        datename = f"[{date_now}] {name}"  # Formátování zprávy
+        with open('chatlog.txt', 'a', encoding='utf-8') as log:  # Otevře log soubor
+            log.write(datename + str(msg) + '\n')  # Zapisuje zprávu do logu
             log.close()
-
-            print(str(data))  # Vypíše zprávu
-        except ConnectionError:
-            print(f"Spojení s klientem {address} bylo ztraceno.")  # Pokud klient spadne, server může zůstat zapnutý
-            # a čeká znovupřipojení
-            time.sleep(1)
-            print(f"Čekám na opětovné připojení...")
+        if msg != bytes("quit", "utf8"):
+            broadcast(msg, datename+": ")
+        else:
+            #client.send(bytes("quit", "utf8"))
+            client.close()
+            print(f'[*]{addresses[client]} se odpojil.')
+            del clients[client]
+            broadcast(bytes(f'{name} se odpojil.', encoding='utf8'))
             break
 
 
-def send(conn, address):
-    while True:
-        try:
-            message = input()  # Pošli zprávu
-            date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Zaznamenání času odeslání zprávy
-            message = f"[{date_now}] {name}: {message}"  # Formátování zprávy
 
-            log = open('chatlog.txt', 'a', encoding='utf-8')  # Otevře log soubor
-            log.write(str(message) + '\n')  # Zapisuje zprávu do logu
-            log.close()
-            conn.send(message.encode())  # Odesílá zprávu klientovi
+def broadcast(msg, prefix=""):  # Pošle zprávu všem klientům
+    try:
+        for sock in clients:
+            sock.send(bytes(prefix, "utf8")+msg)
 
-        except ConnectionError:
-            print('Zpráva nebyla odeslána, klient nenalezen...')
-            break
-
-
-def server_program():
-    server_socket.bind((SERVER_HOST, SERVER_PORT))  # Spojí host adresu a port dohromady
-    server_socket.listen(2)  # Poslouchá pro 'x' zařízení
-
-    print(f"[*] Čekám na připojení {SERVER_HOST}:{SERVER_PORT}")
-    while True:
-        try:
-            conn, address = server_socket.accept()  # Přijme nové připojení
-            print ('prijmuto pripojeni')
-            receiveThread = Thread(target=recv, args=(conn, address))  # Zapne thready s odesíláním a přijímáním zpráv
-            sendThread = Thread(target=send, args=(conn, address))
-            receiveThread.start()
-            sendThread.start()
-        except ConnectionError:
-            break
+    except ConnectionResetError:
+        print(f'Klient se násilně odpojil.')
 
 
 if __name__ == '__main__':
     # IP adresa serveru
+    clients = {}
+    addresses = {}
+    clientslist = []
     SERVER_HOST = "0.0.0.0"
-    SERVER_PORT = 5002
-    name = 'MCC'  # Jméno zobrazené u zprávy (Mission Control Center)
+    SERVER_PORT = 5003
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Pouze přejmenované na server pro lepší vyznání
-    log = open('chatlog.txt', 'a', encoding='utf-8')
-    server_program()  # Spustí celý program
+    server_socket.bind((SERVER_HOST, SERVER_PORT))  # Spojí host adresu a port dohromady
+    server_socket.listen(5)  # Poslouchá pro 'x' zařízení
+
+    ACCEPT_THREAD = Thread(target=accept_connection)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+
+    server_socket.close()
